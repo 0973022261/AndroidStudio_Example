f71b4a79fbb5f873e72821306f9c9c06a2c56370
Hyengmin_In <inhm1234@naver.com>
2016-05-11 16:27:20 +0900

    프로젝트명 : 액티비티간 인텐트 초급연습
    전화걸기 버튼을 클릭시 전화걸기 다이얼을 띄운다.
    메뉴화면 버튼을 클릭시 메뉴화면을 띄운다.
    
    작성자 : 인형민

---
+++ Intent_example/app/src/main/java/com/example/inhm/intent_example/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.inhm.intent_example;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * 2016-05-11
+ *
+ * 프로젝트명 : 액티비티간 인텐트 초급 연습
+ *
+ * 전화걸기 버튼을 누를시 전화걸기 다이얼을 띄운다.
+ * 메뉴화면 버튼을 누를시 메뉴화면을 띄운다.
+ *
+ * 작성자 : 인형민
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_MENU = 1001;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel-010-1234-5678"));
+        startActivity(intent);
+    }
+    public void onButton2Clicked(View v) {
+        Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+        startActivityForResult(intent,REQ_MENU);
+    }
+}
---
+++ Intent_example/app/src/main/java/com/example/inhm/intent_example/MenuActivity.java
@@ -0,0 +1,18 @@
+package com.example.inhm.intent_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void onButton1Clicked(View v) {
+        finish();
+    }
+}
